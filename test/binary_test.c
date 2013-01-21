159d7d5b77532a5eea896edcd1192bab85ea5feb
patito <benatto@gmail.com>
2013-01-21 17:39:14 -0200

    Add test using alloc_type malloc

--- test/binary_test.c
+++ test/binary_test.c
@@ -6,7 +6,7 @@
 
 #include "binary_test.h"
 
-static void malelf_binary_open_TEST(void)
+static void malelf_binary_open_mmap_TEST(void)
 {
        MalelfBinary bin;
        _i32 result;
@@ -26,6 +26,28 @@ static void malelf_binary_open_TEST(void)
        CU_ASSERT(result == MALELF_SUCCESS);
 }
 
+static void malelf_binary_open_malloc_TEST(void)
+{
+       MalelfBinary bin;
+       _i32 result;
+
+       malelf_binary_init(&bin);
+       malelf_binary_set_alloc_type(&bin, MALELF_ALLOC_MALLOC);
+
+       result = malelf_binary_open("test/uninfected", &bin);
+       CU_ASSERT(result == MALELF_SUCCESS);
+       result = malelf_binary_close(&bin);
+       CU_ASSERT(result == MALELF_SUCCESS);
+
+       malelf_binary_init(&bin);
+       
+       result = malelf_binary_open("/wrong/path/uninfected", &bin);
+       CU_ASSERT(result == MALELF_ENOENT);
+       result = malelf_binary_close(&bin);
+       CU_ASSERT(result == MALELF_SUCCESS);
+}
+
+
 CU_ErrorCode binary_get_test_suite(CU_pSuite *rsuite)
 {
 	CU_pSuite suite = NULL;
@@ -40,11 +62,16 @@ CU_ErrorCode binary_get_test_suite(CU_pSuite *rsuite)
 		return CU_get_error();
 	}
 
-	if (NULL == CU_add_test(suite, "malelf_binary_open_TEST", malelf_binary_open_TEST)) {
-		*rsuite = NULL;
-		return CU_get_error();
+	if ((NULL == CU_add_test(suite, 
+                                "malelf_binary_open_mmap_TEST", 
+                                malelf_binary_open_mmap_TEST)) ||
+            (NULL == CU_add_test(suite,
+                                "malelf_binary_open_malloc_TEST",
+                                malelf_binary_open_malloc_TEST))) {
+	        *rsuite = NULL;
+	        return CU_get_error();
 	}
 
-	*rsuite = suite;
-	return CUE_SUCCESS;
+        *rsuite = suite;
+        return CUE_SUCCESS;
 }
