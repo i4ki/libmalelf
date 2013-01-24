eb3e8095a455d6c2c2624dbc16f24c9bcdfdbf5b
patito <benatto@gmail.com>
2013-01-24 17:15:49 -0200

    Add doc in module Ehdr.

--- src/include/malelf/ehdr.h
+++ src/include/malelf/ehdr.h
@@ -1,3 +1,31 @@
+/* 
+ * The malelf library was written in pure C, with the objective to 
+ * provide a quick and easy way a set functions for programmers to 
+ * manipulate ELF files. With libmalelf can dissect and infect ELF 
+ * files. Evil using this library is the responsibility of the programmer.
+ *
+ * Author: Tiago Natel de Moura <tiago4orion@gmail.com>
+ *
+ * Contributor: Daniel Ricardo dos Santos <danielricardo.santos@gmail.com>
+ *              Paulo Leonardo Benatto <benatto@gmail.com>
+ *
+ * Copyright 2012, 2013 by Tiago Natel de Moura. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+
 #ifndef MALELF_EHDR_H
 #define MALELF_EHDR_H
 
@@ -8,12 +36,16 @@
 
 MALELF_BEGIN_DECLS
 
-/*!
- * \file ehdr.h
- * \brief A class used to control the ELF header.
+/*! \file ehdr.h
+ *  \brief MalelfEhdr class stores information about ELF Header Table.
  *
- * The MalelfEhdr union is an opaque data type. It
- * should only be accessed via the following functions. 
+ *  The MalelfEhdr structure will be responsible to store all information 
+ *  about ELF Header Table, like object type, version, entry point,
+ *  program header table offset, section header table offset, ELF
+ *  header size and others. 
+ *
+ *  The MalelfEhdr class is not an opaque data structure, but if you
+ *  prefer, there is getters to access the class members.
  *
  */
 typedef union {
@@ -21,58 +53,196 @@ typedef union {
         Elf64_Ehdr *eh64;    /*!< 64-bits ELF Header */
 } MalelfEhdr;
 
+
+/*! The MalelfEhdrType stores information about e_type member.
+ */
 typedef struct {
         _u16 name;
         _i32 value;
         char *meaning;
 } MalelfEhdrType;
 
+/*! The MalelfEhdrVersion stores information about e_version member.
+ */
 typedef struct {
         _u16 name;
         _i32 value;
         char *meaning;
 } MalelfEhdrVersion;
 
+/*! The MalelfEhdrMachine stores information about e_machine member.
+ */
 typedef struct {
         _u16 name;
         _i32 value;
         char *meaning;
 } MalelfEhdrMachine;
 
+
+/*! Get e_type member from ELF Header Table.
+ *  
+ *  \param ehdr Valid ELF Header Table object.
+ *  \param class The binary architeture class.
+ *  \param me_type Saved values(name, valeu and meaning) MalelfEhdrType.
+ *
+ *  \return MALELF_SUCCESS if MalelfEhdrType was successful set, 
+ *          otherwise returns MALELF_ERROR.  
+ */
 extern _i32 malelf_ehdr_get_type (MalelfEhdr *ehdr, 
                                   _u8 class, 
                                   MalelfEhdrType *me_type);
 
+
+/*! Get e_machine member from ELF Header Table.
+ *  
+ *  \param ehdr Valid ELF Header Table object.
+ *  \param class The binary architeture class.
+ *  \param me_machine Saved values(name, valeu and meaning) MalelfEhdrMachine.
+ *
+ *  \return MALELF_SUCCESS if MalelfEhdrMachine was successful set, 
+ *          otherwise returns MALELF_ERROR.  
+ */
 extern _i32 malelf_ehdr_get_machine(MalelfEhdr *ehdr, 
                                     _u8 class, 
                                     MalelfEhdrMachine *me_machine);
 
+
+/*! Get e_version member from ELF Header Table.
+ *  
+ *  \param ehdr Valid ELF Header Table object.
+ *  \param class The binary architeture class.
+ *  \param me_version Saved values(name, valeu and meaning) MalelfEhdrVersion.
+ *
+ *  \return MALELF_SUCCESS if MalelfEhdrVersion was successful set, 
+ *          otherwise returns MALELF_ERROR.  
+ */
 extern _i32 malelf_ehdr_get_version(MalelfEhdr *ehdr, 
                                     _u8 class, 
                                     MalelfEhdrVersion *version);
 
+
+/*! Get e_shoff member from ELF Header Table.
+ *  
+ *  \param ehdr Valid ELF Header Table object.
+ *  \param class The binary architeture class.
+ *  \param shoff Saved e_shoff from ELF Header Table.
+ *
+ *  \return MALELF_SUCCESS if shoff was successful saved, 
+ *          otherwise returns MALELF_ERROR.  
+ */
 extern _i32 malelf_ehdr_get_shoff(MalelfEhdr *ehdr, _u8 class, _u32 *shoff);
 
+
+/*! Get e_phoff member from ELF Header Table.
+ *  
+ *  \param ehdr Valid ELF Header Table object.
+ *  \param class The binary architeture class.
+ *  \param phoff Saved e_phoff from ELF Header Table.
+ *
+ *  \return MALELF_SUCCESS if phoff was successful saved, 
+ *          otherwise returns MALELF_ERROR.  
+ */
 extern _i32 malelf_ehdr_get_phoff(MalelfEhdr *ehdr, _u8 class, _u32 *pshoff);
 
+
+/*! Get e_phoff member from ELF Header Table.
+ *  
+ *  \param ehdr Valid ELF Header Table object.
+ *  \param class The binary architeture class.
+ *  \param entry Saved e_phoff from ELF Header Table.
+ *
+ *  \return MALELF_SUCCESS if entry was successful saved, 
+ *          otherwise returns MALELF_ERROR.  
+ */
 extern _i32 malelf_ehdr_get_entry(MalelfEhdr *ehdr, _u8 class, _u32 *entry);
 
+
+/*! Set another Ehdr to the Binary.
+ *  
+ *  \param ehdr Valid ELF Header Table object.
+ *  \param class The binary architeture class.
+ *  \param mem The new ELF Header Table.
+ *  \param size The size of new Ehdr.
+ *
+ *  \return MALELF_SUCCESS if entry was successful saved, 
+ *          otherwise returns MALELF_ERROR.  
+ */
 extern _i32 malelf_ehdr_set(MalelfEhdr* ehdr, _u8 class, _u8 *mem, _u32 size);
 
-extern _i32 malelf_ehdr_get_ehsize(MalelfEhdr *ehdr, _u8 class, _u32 *size);
 
+/*! Get e_ehsize member from ELF Header Table.
+ *  
+ *  \param ehdr Valid ELF Header Table object.
+ *  \param class The binary architeture class.
+ *  \param ehsize Saved e_ehsize from ELF Header Table.
+ *
+ *  \return MALELF_SUCCESS if ehsize was successful saved, 
+ *          otherwise returns MALELF_ERROR.  
+ */
+extern _i32 malelf_ehdr_get_ehsize(MalelfEhdr *ehdr, _u8 class, _u32 *ehsize);
+
+
+/*! Get e_phentsize member from ELF Header Table.
+ *  
+ *  \param ehdr Valid ELF Header Table object.
+ *  \param class The binary architeture class.
+ *  \param phentsize Saved e_phentsize from ELF Header Table.
+ *
+ *  \return MALELF_SUCCESS if phentsize was successful saved, 
+ *          otherwise returns MALELF_ERROR.  
+ */
 extern _i32 malelf_ehdr_get_phentsize(MalelfEhdr *ehdr, 
                                       _u8 class, 
                                       _u32 *phentsize);
 
+
+/*! Get e_phnum member from ELF Header Table.
+ *  
+ *  \param ehdr Valid ELF Header Table object.
+ *  \param class The binary architeture class.
+ *  \param phnum Saved e_phnum from ELF Header Table.
+ *
+ *  \return MALELF_SUCCESS if phnum was successful saved, 
+ *          otherwise returns MALELF_ERROR.  
+ */
 extern _i32 malelf_ehdr_get_phnum(MalelfEhdr *ehdr, _u8 class, _u32 *phnum);
 
+
+/*! Get e_shentsize member from ELF Header Table.
+ *  
+ *  \param ehdr Valid ELF Header Table object.
+ *  \param class The binary architeture class.
+ *  \param shentsize Saved e_shentsize from ELF Header Table.
+ *
+ *  \return MALELF_SUCCESS if shentsize was successful saved, 
+ *          otherwise returns MALELF_ERROR.  
+ */
 extern _i32 malelf_ehdr_get_shentsize(MalelfEhdr *ehdr, 
                                       _u8 class, 
                                       _u32 *shentsize);
 
+
+/*! Get e_shnum member from ELF Header Table.
+ *  
+ *  \param ehdr Valid ELF Header Table object.
+ *  \param class The binary architeture class.
+ *  \param shnum Saved e_shnum from ELF Header Table.
+ *
+ *  \return MALELF_SUCCESS if shnum was successful saved, 
+ *          otherwise returns MALELF_ERROR.  
+ */
 extern _i32 malelf_ehdr_get_shnum(MalelfEhdr *ehdr, _u8 class, _u32 *shnum);
 
+
+/*! Get e_shstrndx member from ELF Header Table.
+ *  
+ *  \param ehdr Valid ELF Header Table object.
+ *  \param class The binary architeture class.
+ *  \param shstrndx Saved e_shstrndx from ELF Header Table.
+ *
+ *  \return MALELF_SUCCESS if shstrndx was successful saved, 
+ *          otherwise returns MALELF_ERROR.  
+ */
 extern _i32 malelf_ehdr_get_shstrndx(MalelfEhdr *ehdr, 
                                      _u8 class, 
                                      _u32 *shstrndx);
