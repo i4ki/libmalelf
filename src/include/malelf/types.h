c784e54f973cdd782144037394480d9dc5f69162
patito <benatto@gmail.com>
2013-01-25 13:51:57 -0200

    Added description/license in all files.

--- src/include/malelf/types.h
+++ src/include/malelf/types.h
@@ -1,3 +1,30 @@
+/* 
+ * The malelf library was written in pure C, with the objective to 
+ * provide a quick and easy way a set functions for programmers to 
+ * manipulate ELF files. With libmalelf can dissect and infect ELF 
+ * files. Evil using this library is the responsibility of the programmer.
+ *
+ * Author: Tiago Natel de Moura <tiago4orion@gmail.com>
+ *
+ * Contributor: Daniel Ricardo dos Santos <danielricardo.santos@gmail.com>
+ *              Paulo Leonardo Benatto <benatto@gmail.com>
+ *
+ * Copyright 2012, 2013 by Tiago Natel de Moura. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
 #ifndef TYPES_H
 #define TYPES_H
 
