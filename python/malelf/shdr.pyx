bc2868294920fa28d862456e4e5a1ecb01562a59
Tiago Natel de Moura <tiago4orion@gmail.com>
2013-09-13 21:48:31 -0300

    Old python binding deleted

--- python/malelf/shdr.pyx
+++
@@ -1,6 +0,0 @@
-cimport cshdr
-
-cdef class MalelfShdr:
-    cdef cshdr.MalelfShdr _c_shdr
-    cdef cshdr.MalelfShdrType _c_shdr_t
-
--- python/test_ehdr.py
+++
@@ -1,8 +0,0 @@
-from malelf.binary import MalelfBinary
-from malelf.ehdr import MalelfEhdr
-
-binary = MalelfBinary()
-print binary.open("/bin/ls")
-ehdr = MalelfEhdr()
-print ehdr.get_type()
-binary.close()
