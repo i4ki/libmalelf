176a86181994f342af5b166a17e58c8d811c3b1f
Tiago Natel de Moura <tiago4orion@gmail.com>
2014-01-10 09:36:29 -0200

    Added travis-ci image status to README.md and return for number of tests failed

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
+[![Build Status](https://travis-ci.org/SecPlus/libmalelf.png?branch=master)](https://travis-ci.org/SecPlus/libmalelf)
+
 libmalelf
 =========
 
--- test/main_test.c
+++ test/main_test.c
@@ -46,33 +46,38 @@ CU_ErrorCode infect_get_test_suite(CU_pSuite *rsuite);
 
 int main()
 {
-   CU_pSuite binary_suite = NULL;
-   CU_pSuite ehdr_suite = NULL;
-   CU_pSuite report_suite = NULL;
-   CU_pSuite debug_suite = NULL;
-   CU_pSuite util_suite = NULL;
-   CU_pSuite infect_suite = NULL;
+        CU_pSuite binary_suite = NULL;
+        CU_pSuite ehdr_suite = NULL;
+        CU_pSuite report_suite = NULL;
+        CU_pSuite debug_suite = NULL;
+        CU_pSuite util_suite = NULL;
+        CU_pSuite infect_suite = NULL;
 
-   /* initialize the CUnit test registry */
-   if (CUE_SUCCESS != CU_initialize_registry()) {
-      return CU_get_error();
-   }
+        /* initialize the CUnit test registry */
+        if (CUE_SUCCESS != CU_initialize_registry()) {
+                return CU_get_error();
+        }
 
-   /* add a suite to the registry */
-   if (CUE_SUCCESS != ehdr_get_test_suite(&ehdr_suite) ||
-       CUE_SUCCESS != report_get_test_suite(&report_suite) ||
-       CUE_SUCCESS != debug_get_test_suite(&debug_suite) ||
-       CUE_SUCCESS != util_get_test_suite(&util_suite) ||
-       CUE_SUCCESS != binary_get_test_suite(&binary_suite) ||
-       CUE_SUCCESS != infect_get_test_suite(&infect_suite)) {
-           fprintf(stderr, "Failed to create test suite.\n");
-           CU_cleanup_registry();
-           return 1;
-   }
+        /* add a suite to the registry */
+        if (CUE_SUCCESS != ehdr_get_test_suite(&ehdr_suite) ||
+            CUE_SUCCESS != report_get_test_suite(&report_suite) ||
+            CUE_SUCCESS != debug_get_test_suite(&debug_suite) ||
+            CUE_SUCCESS != util_get_test_suite(&util_suite) ||
+            CUE_SUCCESS != binary_get_test_suite(&binary_suite) ||
+            CUE_SUCCESS != infect_get_test_suite(&infect_suite)) {
+                fprintf(stderr, "Failed to create test suite.\n");
+                CU_cleanup_registry();
+                return 1;
+        }
 
-   /* Run all tests using the CUnit Basic interface */
-   CU_basic_set_mode(CU_BRM_VERBOSE);
-   CU_basic_run_tests();
-   CU_cleanup_registry();
-   return CU_get_number_of_tests_failed();
+        /* Run all tests using the CUnit Basic interface */
+        CU_basic_set_mode(CU_BRM_VERBOSE);
+        CU_basic_run_tests();
+
+        if (CU_get_number_of_tests_failed() > 0) {
+                CU_cleanup_registry();
+                return 1;
+        }
+
+        return 0;
 }
