47a3cb1364c557312408d453bc542235f88f35a3
Rafael Escrich <rafaelescrich@gmail.com>
2014-01-15 18:30:13 -0200

    partial fix of elf machine bug

--- src/ehdr.c
+++ src/ehdr.c
@@ -54,15 +54,8 @@ static MalelfEhdrTable _me_version[] = {
 };
 
 static MalelfEhdrTable _me_machine[] = {
-        {"EM_NONE",  0, "No machine"},
-        {"EM_M32",   1, "AT&T WE 32100"},
-        {"EM_SPARC", 2, "SPARC"},
-        {"EM_386",   3, "Intel 80386"},
-        {"EM_68K",   4, "Motorola 68000"},
-        {"EM_88K",   5, "Motorola 88000"},
-        {"EM_860",   7, "Intel 80860"},
-        {"EM_MIPS",  8, "MIPS RS3000"},
-        {"UNKNOWN", 0, "UNKNOWN"}
+#include "elf_machines.inc"
+        {"UNKNOWN", 201, "UNKNOWN"}
 };
 
 /* static methods */
